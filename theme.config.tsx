2f0c156272cd66e025e83c2414b4d0011eedd653
Your Name <your.email@example.com>
2026-02-27 10:21:11 +0800

    Initial commit with Nextra setup

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>OpenClaw Wiki</span>,
+  project: {
+    link: 'https://github.com/279458179/openclaw_wiki',
+  },
+  docsRepositoryBase: 'https://github.com/279458179/openclaw_wiki/tree/main',
+  footer: {
+    text: 'OpenClaw Wiki',
+  },
+}
+
+export default config
